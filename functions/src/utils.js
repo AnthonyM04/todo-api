85c0159f3454176c92f4866016919f95b3690c4c
anthony* <72421281+AnthonyM04@users.noreply.github.com>
2023-02-16 09:07:15 -0500

    put update back to done

--- functions/src/utils.js
+++ functions/src/utils.js
@@ -1,19 +1,19 @@
 import { initializeApp, cert, getApps } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
-import {service_account} from "./service_account.js";
+import service_account from "./service_account.json" assert { type: "json" };
 
-export function getFirestoreInstance() {
-    //check if app has already been initialized
-    try{
-        const isInitialized = getApps().length > 0;
-        if (!isInitialized) { // not initialized, connect to firebase
-            initializeApp({
-                credential: cert(service_account)
-            });
-        }
-        return getFirestore();
+export async function getFirestoreInstance() {
+  // check if app has already been initialized
+  try {
+    const isInitialized = getApps().length > 0;
+    if (!isInitialized) { // not initialized, connect to firebase
+      initializeApp({
+        credential: cert(service_account),
+      });
     }
-    catch(error){
-
-    }
+    return getFirestore();
+  }
+  catch (error) {
+    console.error(error);
+  }
 }
